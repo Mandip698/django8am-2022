fc7af92a8bc60fc6d3a1edc43a77a9e982b1e406
Mandip698 <mandip698@gmail.com>
2022-07-12 20:17:44 +0545

    Update

---
+++ news/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'pages/home/index.html')
+
+
+def about(request):
+    return render(request,'pages/about/about.html')
+
+
+def contact(request):
+    return render(request,'pages/contact/contact.html')
